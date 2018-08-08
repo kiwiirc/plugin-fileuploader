b312ae93ddfc90bbfdeb043ba4b420cf6e670cb2
Scott <smcgann@cantelope.ml>
2018-08-08 09:36:12 -0700

    added babel support

--- fileuploader-kiwiirc-plugin/webpack.config.js
+++ fileuploader-kiwiirc-plugin/webpack.config.js
@@ -1,20 +1,27 @@
 const path = require('path');
 
 module.exports = {
-    mode: 'production',
-    entry: './plugin.js',
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: [ 'style-loader', 'css-loader' ]
-            }
-        ]
+  mode: 'development',
+  entry: './plugin.js',
+  module: {
+    rules: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader',
+      query: {
+        presets: ['es2015'],
+      }
     },
-    devtool: 'source-map',
-    devServer: {
-        contentBase: path.join(__dirname, "dist"),
-        compress: true,
-        port: 9000
-    }
+      {
+        test: /\.css$/,
+        use: [ 'style-loader', 'css-loader' ]
+      }
+    ]
+  },
+  devtool: 'source-map',
+  devServer: {
+      contentBase: path.join(__dirname, "dist"),
+      compress: true,
+      port: 9000
+  }
 };
