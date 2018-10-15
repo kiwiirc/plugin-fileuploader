4e2a2a06d5dfcbd62a458f592e49bb6a7b61f975
Jason Papakostas <vithos@gmail.com>
2018-10-15 16:04:10 -0500

    add editorconfig and reformat files

--- fileuploader-kiwiirc-plugin/webpack.config.js
+++ fileuploader-kiwiirc-plugin/webpack.config.js
@@ -1,32 +1,32 @@
 const path = require('path');
 
 module.exports = {
-  mode: 'production',
-  entry: './plugin.js',
-  output: {
-    filename: 'plugin-fileuploader.js',
-  },
-  module: {
-    rules: [{
-      test: /\.js$/,
-      exclude: /node_modules/,
-      loader: 'babel-loader',
-      query: {
-        presets: ['env'],
-      }
+    mode: 'production',
+    entry: './plugin.js',
+    output: {
+        filename: 'plugin-fileuploader.js',
     },
-      {
-        test: /\.css$/,
-        use: [ 'style-loader', 'css-loader' ]
-      }
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+            query: {
+                presets: ['env'],
+            }
+        },
+        {
+            test: /\.css$/,
+            use: [ 'style-loader', 'css-loader' ]
+        }
     ]
-  },
-  devtool: 'source-map',
-  devServer: {
+},
+devtool: 'source-map',
+devServer: {
     filename: 'plugin-fileuploader.js',
     contentBase: path.join(__dirname, 'dist'),
     compress: true,
     host: process.env.HOST || 'localhost',
     port: 41040,
-  }
+}
 };
