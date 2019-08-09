fc7a71b0eb27317946edc2273e91e6cd82cf7b9e
ItsOnlyBinary <ItsOnlyBinary@users.noreply.github.com>
2019-08-09 19:58:15 +0000

    Fix missing function param

--- fileuploader-kiwiirc-plugin/src/handlers/show-dashboard-on-drag-enter.js
+++ fileuploader-kiwiirc-plugin/src/handlers/show-dashboard-on-drag-enter.js
@@ -1,6 +1,6 @@
 import { getValidUploadTarget } from '../utils/get-valid-upload-target'
 
-export function showDashboardOnDragEnter(kiwiApi) {
+export function showDashboardOnDragEnter(kiwiApi, dashboard) {
     return function handleDragEnter(/* event */) {
         // swallow error and ignore drag if no valid buffer to share to
         try {
